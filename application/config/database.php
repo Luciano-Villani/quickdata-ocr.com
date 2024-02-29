9b207db91c82abf383f12d59197306dc4ee5cebb
luciano-villani <lucianov@visualdigital.com.ar>
2024-02-29 17:29:46 -0300

    nuedata

--- application/config/database.php
+++ application/config/database.php
@@ -27,7 +27,7 @@ switch ($_SERVER['REMOTE_ADDR']) {
    
 		default:
 
-			$host = '34.123.36.213';
+			$host = '34.125.217.72';
 			$userdb ='u117285061_mvl_ocr_db';
 			$pass = '/GaTGjBg7#bB';
 			$base ='u117285061_mvl_ocr_db';
