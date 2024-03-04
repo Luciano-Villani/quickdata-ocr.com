ea377baaa51ce3e24ccd0fa784d362788c9801ba
luciano-villani <lucianov@visualdigital.com.ar>
2024-03-04 09:01:32 -0300

    css3

--- application/views/manager/secciones/consolidados/listados_0403.php
+++ application/views/manager/secciones/consolidados/listados_0403.php
@@ -36,18 +36,6 @@
 	.select2-search:after {
 		content: '' !important;
 	}
-	.card {
-		container {
- 		 padding-left: 0 !important;
- 		 padding-right: 0 !important;
-		  width: 100% !important;
-}
-	}
-	.tablas {
-		width: 1300px !important;
-		margin-left: -80px; !important;
-	}
-
 </style>
 <div class="card">
 <h5 class="card-title bg-titulo text-center text-dark"> Filtros y Descarga de Reportes </h5>
@@ -245,5 +233,6 @@
 </div>
 
 <script>
+
 
 </script>
