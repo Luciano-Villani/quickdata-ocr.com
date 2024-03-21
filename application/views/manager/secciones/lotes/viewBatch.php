6b2c20c855917ba6804c2d2fae7cf73f6379c37d
luciano-villani <lucianov@visualdigital.com.ar>
2024-03-21 14:48:05 -0300

    jueves

--- application/views/manager/secciones/lotes/viewBatch.php
+++ application/views/manager/secciones/lotes/viewBatch.php
@@ -1,16 +1,30 @@
+<style>
+	
+	.card {
+		container {
+ 		 padding-left: 0 !important;
+ 		 padding-right: 0 !important;
+		  width: 100% !important;
+	}
+	}
+	.tablas2 {
+		width: 1650px !important;
+		margin-left: -270px !important;
 
-<div class="container-fuid card">
-	<div class="card-header header-elements-inline">
-		<h5 class="card-title">Datos en base - Lotes</h5>
-		<p>mostrar datos del lote.</p>
+	}
+</style>
+<div class="container-fuid card tablas2 px-2">
+	<div class="card-header header-elements-inline ">
+		<h5 class="card-title text-center ">Lecturas de facturas - Archivos</h5>
+		<p>Datos del lote.</p>
 	</div>
-	<table class="datatable-ajax display table-bordered table-hover datatable-highlight no-footer  " style="width: auto">
+	<table class="datatable-ajax display table-bordered table-hover datatable-highlight no-footer">
 		<thead>
         <tr>
 					<th>Cuenta</th>
 					<th>Medidor</th>
 					<th>Nro de factura</th>
-					<th>Pedríodo</th>
+					<th>Período</th>
 					<th>Fecha emision</th>
 					<th>Vencimiento</th>
 					<th>Total importe</th>
