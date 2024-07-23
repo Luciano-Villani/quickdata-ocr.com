9ed74723ac9edbccb9ac3708c954998764a278bb
luciano-villani <lucianov@visualdigital.com.ar>
2024-07-23 20:12:09 -0300

    js

--- assets/manager/js/secciones/lotes/viewBatch.js
+++ assets/manager/js/secciones/lotes/viewBatch.js
@@ -3,19 +3,41 @@
 $(document).ready(function () {
 
   var base_url = $("body").data("base_url");
+
   var mytable =   $(".datatable-ajax").dataTable({
-    
+    select: true,
+    select: {
+      'style': 'multi',
+      'selector': 'td:first-child'
+    },
     dom: 'Blfrtip',
     buttons: [
       'colvis'
   ],
     pageLength: 10,
     language: {
+      select: {
+        rows: " %d Registros seleccionados",
+      },
       url: base_url + "assets/manager/js/plugins/tables/translate/spanish.json",
     },
 
-    order: [[0, 'desc']],
+    order: [[1, 'desc']],
     columnDefs: [
+      {visible:false,targets:[13]},
+      {
+        'targets': 0,
+        'visible':false,
+        'checkboxes': {
+          'selectRow': true,
+        },
+        orderable: false,
+        // render:function(data, type, row, meta){
+        //   console.log(data);
+
+        // },
+        }
+      
       // {className: "d-none", targets:[8]},
       // { className: 'dt-nowrap', targets: [ 8 ] },
       // { width: '1%',visible:false, targets: [ 0 ] },
@@ -40,6 +62,14 @@ $(document).ready(function () {
       error: function (jqXHR, textStatus, errorThrown) {
         alert(jqXHR.status + textStatus + errorThrown);
       },
+    },
+    createdRow: function (row, data, dataIndex) {
+      // agrego el atributo id al td 0
+      console.log('createrow');
+      // console.log(data);      // console.log(row);
+      // return;
+      $(row).attr("id", data[13]);
+      $(row).find("td:eq(0)").attr("id", data[13]);
     },
     initComplete: function () {
       this.api()
@@ -69,6 +99,15 @@ $(document).ready(function () {
   });
   // $(".datatable-ajax").DataTable().columns.adjust().draw();
   // $(".datatable-ajax").DataTable().responsive.recalc();
+
+  mytable.on( 'select', function ( e, dt, type, indexes ) {
+  console.log(dt);
+    if ( type === 'row' ) {
+        var x = table.rows().data().pluck('Stek');
+        document.getElementById('selectedStek').value = x; 
+        document.getElementById("selectedStek").style.color = "blue";
+    }
+  });
   $("body").on("click", "span.mergefile", function (e) {
     var file = $(this).data("file");
     e.preventDefault();
@@ -198,9 +237,233 @@ $(document).ready(function () {
     });
   });
   
+ 
+  $("body").on("click", "span.reload-lote", function (e) {
+
+    e.preventDefault();
+    var dato = new FormData();
+
+    dato.append("file", $(this).data("file"));
+    dato.append("id_proveedor", $(this).data("id_proveedor"));
+
+    $.confirm({
+      autoClose: "cancel|10000",
+      title: "Recargar Datos",
+      content: "Confirma ?",
+      buttons: {
+        confirm: {
+          text: "Recargar datos API",
+          btnClass: "btn-blue",
+          action: function () {
+            $.ajax({
+              type: "POST",
+              contentType: false,
+              dataType: "json",
+              data: dato,
+              processData: false,
+              cache: false,
+              beforeSend: function () {},
+              url: $("body").data("base_url") + "Lotes/getDato",
+              success: function (result) {
+                alertas(result);
+        
+              //  $( ".datatable-ajax").DataTable().ajax.reload()
+              },
+              error: function (xhr, errmsg, err) {
+                console.log(xhr.status + ": " + xhr.responseText);
+              },
+            });
+          },
+        },
+        cancel: {
+          text: "Cancelar",
+          btnClass: "btn-red",
+          action: function () {},
+        },
+      },
+    });
+  });
+  
   
 
 });
+
+
+
+var Elemento_Module = {
+	attrs: {
+		publicaciones: [],
+
+	},
+	methods: function () {
+
+
+
+		function checkAvailableListPublicaciones(id) {
+			return Elemento_Module.attrs.publicaciones.includes(id) ? false : true;
+		}
+
+		function getAll() {
+			return Elemento_Module.attrs.publicaciones;
+		}
+
+		function countList() {
+			return Elemento_Module.attrs.publicaciones.length;
+		}
+
+		function display() {
+			$('.publicaciones_seleccionadas').text(this.countList());
+			console.log('[array publicaciones] - Lista de ids', this.getAll());
+		}
+
+		function onChangeNumberTextSelect() {
+			$('.publicaciones_seleccionadas').text(this.countList());
+		}
+
+		function actionsButtons() {
+
+			$('.remove-all-Publicaciones').click(function (e) {
+				e.preventDefault();
+
+				swal({
+					title: 'Eliminar Publicaciones',
+					text: "¿Esta seguro desea eliminar la(s) persona(s) seleccionada(s)?",
+					type: 'error',
+					showCancelButton: true,
+					confirmButtonText: 'Si, eliminar',
+					cancelButtonText: 'No, cancelar!',
+					confirmButtonClass: 'btn btn-success',
+					cancelButtonClass: 'btn btn-danger m-l-10',
+					buttonsStyling: false
+				}).then(function () {
+
+					Elemento_Module.methods().multiDeletes();
+				}, function () {
+					swal("Cancelado!", "Operación cancelada por el usuario!", "error");
+				});
+			});
+		}
+
+
+
+		function insert(id) {
+
+			if (this.checkAvailableListPublicaciones(id)) {
+				Elemento_Module.attrs.publicaciones.push(id);
+			}
+
+			this.display();
+			this.refresh();
+		}
+
+		function remove(id) {
+
+			Elemento_Module.attrs.publicaciones.splice(Elemento_Module.attrs.publicaciones.indexOf(id), 1);
+
+			this.display();
+			this.refresh();
+		}
+
+		function refresh() {
+
+			if (Elemento_Module.attrs.publicaciones.length) {
+
+				this.onChangeNumberTextSelect();
+			} else {
+
+			}
+
+			$('.publicaciones_seleccionadas').html(this.countList());
+		}
+
+		function clearAll() {
+			Elemento_Module.attrs.publicaciones = [];
+			this.display();
+		}
+
+		function clearAndRefresh() {
+			Elemento_Module.methods().clearAll();
+
+			Elemento_Module.methods().refresh();
+		}
+
+		function onAllSelectionPublicaciones() {
+			this.clearAll();
+
+		}
+
+		function fetchIds(ids) {
+			this.clearAndRefresh();
+
+			$('.publicaciones_seleccionadas').html("");
+
+			ids.map(r => Elemento_Module.methods().insert(r));
+		}
+
+		return {
+			fetchIds: fetchIds,
+			insert: insert,
+			remove: remove,
+			getAll: getAll,
+			display: display,
+			refresh: refresh,
+			countList: countList,
+			clearAll: clearAll,
+			clearAndRefresh: clearAndRefresh,
+			checkAvailableListPublicaciones: checkAvailableListPublicaciones,
+			onChangeNumberTextSelect: onChangeNumberTextSelect,
+			onAllSelectionPublicaciones: onAllSelectionPublicaciones
+		};
+	},
+	init: function () {
+		console.log('Module Publicaciones Loaded');
+
+	}
+};
+
+$(document).ready(function () {
+
+  $( ".datatable-ajax").DataTable().on('select', function (e, dt, type, indexes) {
+
+		if (type === 'row') {
+
+			var data =$(".datatable-ajax").DataTable().rows(indexes).data('id');
+			console.log('sdata');
+			console.table(data);
+			Elemento_Module.methods().insert(parseInt(data[0][13]));
+
+		}
+	});
+
+	$( ".datatable-ajax").DataTable().on('deselect', function (e, dt, type, indexes) {
+		if (type === 'row') {
+			var data = $( ".datatable-ajax").DataTable().rows(indexes).data('id');
+			Elemento_Module.methods().remove(parseInt(data[0][13]));
+
+		}
+	});
+
+	$("#selectAllPost").on("click", function (e) {
+
+		if ($(this).is(":checked")) {
+			$( ".datatable-ajax").DataTable().rows().select();
+			$( ".datatable-ajax").DataTable().rows().iterator('row', function (context, index) {
+
+				$(this.row(index).node()).hasClass('selected');
+				var id = $(this.row(index).node()).attr('id');
+				console.log('->' + $(this.row(index).node()).attr('id'));
+
+				Elemento_Module.methods().insert(parseInt(id));
+
+			});
+		} else {
+
+			$( ".datatable-ajax").DataTable().rows().deselect();
+			Elemento_Module.methods().clearAndRefresh();
+			Elemento_Module.methods().countList();
+		}
+	});
+}); 
 
 
 
