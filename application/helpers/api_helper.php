c98dc1c568ef44e163790573e92b563249e8c0a4
luciano-villani <lucianov@visualdigital.com.ar>
2024-10-01 15:12:40 -0300

    lec_azure

--- application/helpers/api_helper.php
+++ application/helpers/api_helper.php
@@ -47,7 +47,7 @@ if (!function_exists('apiRest')) {
                 $operation_url = trim($matches[1]);
 
                 do {
-                    sleep(6);
+                    sleep(2);
 
                     $ch = curl_init();
                     curl_setopt_array($ch, array(
